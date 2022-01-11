a219a2d68f819b4666008772a5aa0cfa7eaab243
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-01-12 00:05:02 +0100

    up

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/transformations/junit/AssertThatTransformation.java
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/transformations/junit/AssertThatTransformation.java
@@ -7,13 +7,29 @@ import spoon.reflect.declaration.CtType;
 import spoon.reflect.reference.CtExecutableReference;
 import xyz.keksdose.spoon.code_solver.history.Change;
 import xyz.keksdose.spoon.code_solver.history.ChangeListener;
+import xyz.keksdose.spoon.code_solver.history.MarkdownString;
+import xyz.keksdose.spoon.code_solver.transformations.BadSmell;
 import xyz.keksdose.spoon.code_solver.transformations.ImportHelper;
 import xyz.keksdose.spoon.code_solver.transformations.TransformationProcessor;
 
 public class AssertThatTransformation extends TransformationProcessor<CtInvocation<?>> {
 
 	private static final String ORG_HAMCREST_MATCHER_ASSERT = "org.hamcrest.MatcherAssert";
+	private static final BadSmell ASSERT_THAT_BAD_SMELL = new BadSmell() {
 
+		@Override
+		public MarkdownString getDescription() {
+			String rawText = "AssertThat in Junit 4 is deprecated. Use AssertThat in Hamcrest instead.";
+			String markdownText = "`AssertThat` in Junit 4 is deprecated. Use `AssertThat` in Hamcrest instead.";
+			return MarkdownString.fromMarkdown(rawText, markdownText);
+		}
+
+		@Override
+		public MarkdownString getName() {
+			return MarkdownString.fromRaw("JUnit4-AssertThat");
+		}
+
+	};
 	public AssertThatTransformation(ChangeListener listener) {
 		super(listener);
 	}
@@ -31,8 +47,13 @@ public class AssertThatTransformation extends TransformationProcessor<CtInvocati
 	private void notifyChangeListener(CtInvocation<?> invocation) {
 		CtType<?> parentType = invocation.getParent(CtType.class);
 		setChanged(parentType,
-			new Change(String.format("Replaced `Assert.assertThat` with `MatcherAssert.assertThat` in `%s`",
-				invocation.getParent(CtExecutable.class).getSimpleName()), "assertThat", parentType));
+			new Change(ASSERT_THAT_BAD_SMELL,
+				MarkdownString.fromMarkdown(
+					String.format("Replaced Assert.assertThat with MatcherAssert.assertThat in %s",
+						invocation.getParent(CtExecutable.class).getSimpleName()),
+					String.format("Replaced `Assert.assertThat` with `MatcherAssert.assertThat` in `%s`",
+						invocation.getParent(CtExecutable.class).getSimpleName())),
+				parentType));
 	}
 
 	private void adjustImports(CtInvocation<?> invocation) {
