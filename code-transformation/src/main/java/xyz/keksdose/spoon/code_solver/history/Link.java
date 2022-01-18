2dab31828e39cbbc15296fffdcaa3406d79116ab
MartinWitt <wittlinger.martin@gmail.com>
2022-01-18 22:13:00 +0100

    feat: transform array.toString() (#35)

---
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/history/Link.java
@@ -0,0 +1,16 @@
+
+package xyz.keksdose.spoon.code_solver.history;
+
+public class Link {
+
+	private String url;
+
+	public Link(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return url;
+	}
+}
