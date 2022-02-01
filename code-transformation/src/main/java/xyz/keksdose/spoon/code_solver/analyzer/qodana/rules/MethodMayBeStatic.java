2cc0a1632fda5f981043203f00fe9b0b06dd1ca9
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-02-01 22:16:26 +0100

    up

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/analyzer/qodana/rules/MethodMayBeStatic.java
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/analyzer/qodana/rules/MethodMayBeStatic.java
@@ -6,10 +6,18 @@ import java.util.LinkedHashSet;
 import java.util.List;
 
 import com.contrastsecurity.sarif.Result;
+import com.google.common.flogger.FluentLogger;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import spoon.reflect.code.CtExecutableReferenceExpression;
+import spoon.reflect.code.CtTypeAccess;
+import spoon.reflect.declaration.CtExecutable;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.declaration.ModifierKind;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.reflect.CtExtendedModifier;
 import xyz.keksdose.spoon.code_solver.history.Change;
 import xyz.keksdose.spoon.code_solver.history.ChangeListener;
@@ -18,6 +26,7 @@ import xyz.keksdose.spoon.code_solver.transformations.BadSmell;
 
 public class MethodMayBeStatic extends AbstractRefactoring {
 
+	private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 	private static final BadSmell METHOD_MAY_BE_STATIC = new BadSmell() {
 		@Override
 		public MarkdownString getName() {
@@ -40,11 +49,13 @@ public class MethodMayBeStatic extends AbstractRefactoring {
 			return;
 		}
 		for (CtMethod<?> method : type.getMethods()) {
-			if (method.getPosition().getSourceStart() == result.getLocations()
-					.get(0)
-					.getPhysicalLocation()
-					.getRegion()
-					.getCharOffset()) {
+			if (getSourceStart(method) == getSourceStartOfResult()) {
+				if (method.isStatic() && method.isPrivate()) {
+					continue;
+				}
+				List<CtExecutableReferenceExpression<?, CtTypeAccess<?>>> executableRefs = type
+						.getElements(new TypeFilter<>(CtExecutableReferenceExpression.class));
+				refactorExecutableReferences(method, executableRefs);
 				LinkedHashSet<CtExtendedModifier> modifiers = new LinkedHashSet<>(method.getExtendedModifiers());
 				modifiers.add(CtExtendedModifier.explicit(ModifierKind.STATIC));
 				method.setExtendedModifiers(modifiers);
@@ -57,9 +68,40 @@ public class MethodMayBeStatic extends AbstractRefactoring {
 		}
 	}
 
+	private void refactorExecutableReferences(CtMethod<?> method,
+			List<CtExecutableReferenceExpression<?, CtTypeAccess<?>>> executableRefs) {
+		for (CtExecutableReferenceExpression<?, CtTypeAccess<?>> executableRef : executableRefs) {
+			if (executableRef.getExecutable() != null) {
+				CtExecutable<?> exec = executableRef.getExecutable().getExecutableDeclaration();
+				if (exec instanceof CtMethod) {
+					CtMethod<?> methodRef = (CtMethod<?>) exec;
+					if (methodRef.equals(method)) {
+						CtTypeReference<Object> typeReference = method.getFactory()
+								.createSimplyQualifiedReference(method.getDeclaringType().getQualifiedName());
+						executableRef.setTarget(method.getFactory().createTypeAccess(typeReference));
+					}
+				}
+			}
+		}
+	}
+
+	private Integer getSourceStartOfResult() {
+		return result.getLocations().get(0).getPhysicalLocation().getRegion().getCharOffset();
+	}
+
+	private int getSourceStart(CtMethod<?> method) {
+		try {
+			return (int) FieldUtils.readField(method.getPosition(), "sourceStart", true);
+		}
+		catch (Throwable e) {
+			logger.atSevere().withCause(e).log("Error while getting source start");
+			return -1;
+		}
+	}
+
 	@Override
 	public List<BadSmell> getHandledBadSmells() {
-		return List.of();
+		return List.of(METHOD_MAY_BE_STATIC);
 	}
 
 }
