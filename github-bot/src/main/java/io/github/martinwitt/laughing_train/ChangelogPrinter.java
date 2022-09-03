d63bcff1af69eb8c431d26b6212c1ec386ec93b9
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-09-03 17:57:58 +0200

    qodana

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/ChangelogPrinter.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/ChangelogPrinter.java
@@ -45,14 +45,15 @@ public class ChangelogPrinter {
         Set<String> ruleIds =
                 config.getActiveRules().stream().map(QodanaRules::getRuleId).collect(Collectors.toSet());
         List<AnalyzerResult> activeRuleResults =
-                results.stream().filter(v -> ruleIds.contains(v.ruleID())).collect(Collectors.toList());
+                results.stream().filter(v -> ruleIds.contains(v.ruleID())).toList();
         StringBuilder sb = new StringBuilder();
         sb.append("# Bad smells\n");
         sb.append(String.format("I found %s bad smells:", activeRuleResults.size()))
                 .append("\n");
         for (AnalyzerResult result : activeRuleResults) {
 
-            sb.append("## " + result.ruleID())
+            sb.append("## ")
+                    .append(result.ruleID())
                     .append("\n")
                     .append(result.messageMarkdown())
                     .append("\n")
