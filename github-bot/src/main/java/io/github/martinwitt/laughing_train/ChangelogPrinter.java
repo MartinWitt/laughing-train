e3dec4117353b510fcf467dc40adc9b8342d890e
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-09-03 17:04:14 +0200

    up

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/ChangelogPrinter.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/ChangelogPrinter.java
@@ -2,15 +2,22 @@ package io.github.martinwitt.laughing_train;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import xyz.keksdose.spoon.code_solver.analyzer.qodana.AnalyzerResult;
+import xyz.keksdose.spoon.code_solver.analyzer.qodana.QodanaRules;
 import xyz.keksdose.spoon.code_solver.history.Change;
 
 @ApplicationScoped
 public class ChangelogPrinter {
 
     @Inject
-    private MarkdownPrinter markdownPrinter;
+    MarkdownPrinter markdownPrinter;
+
+    @Inject
+    Config config;
 
     String printChangeLog(List<Change> changes) {
         StringBuilder sb = new StringBuilder();
@@ -28,6 +35,31 @@ public class ChangelogPrinter {
                         "## " + v.getName().asText() + "\n")
                 .append(v.getDescription().asMarkdown())
                 .append("\n"));
+        return sb.toString();
+    }
+
+    public String printResults(List<AnalyzerResult> results) {
+        Set<String> ruleIds =
+                config.getActiveRules().stream().map(QodanaRules::getRuleId).collect(Collectors.toSet());
+        List<AnalyzerResult> activeRuleResults =
+                results.stream().filter(v -> ruleIds.contains(v.ruleID())).collect(Collectors.toList());
+        StringBuilder sb = new StringBuilder();
+        sb.append("# Bad smells\n");
+        sb.append(String.format("I found %s bad smells:", activeRuleResults.size()))
+                .append("\n");
+        for (AnalyzerResult result : activeRuleResults) {
+
+            sb.append("## " + result.ruleID())
+                    .append("\n")
+                    .append(result.messageMarkdown())
+                    .append("\n")
+                    .append("in ")
+                    .append(markdownPrinter.toMarkdown(result.filePath()))
+                    .append("\n")
+                    .append("### Snippet")
+                    .append("\n")
+                    .append(markdownPrinter.toJavaMarkdownBlock(result.snippet()));
+        }
         return sb.toString();
     }
 }
