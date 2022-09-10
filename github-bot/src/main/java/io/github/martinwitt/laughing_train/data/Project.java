015d34016441db66cfae438228cd2e4158f8189e
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-09-11 01:21:28 +0200

    up

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
@@ -2,5 +2,14 @@ package io.github.martinwitt.laughing_train.data;
 
 import java.io.File;
 import java.io.Serializable;
+import org.apache.commons.lang3.StringUtils;
 
-public record Project(String name, String url, File folder) implements Serializable {}
+public record Project(String name, String url, File folder) implements Serializable {
+    public String getOwnerRepoName() {
+        String[] split = StringUtils.split(name(), "/");
+        if (split.length == 2) {
+            return split[0] + "/" + split[1];
+        }
+        return split[split.length - 2] + "/" + split[split.length - 1];
+    }
+}
