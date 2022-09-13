b91c7ba2a0e4a773079fd54b9e9e720035ceecf1
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-09-13 17:17:56 +0200

    change to more stable closeable

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
@@ -1,14 +1,10 @@
 package io.github.martinwitt.laughing_train.data;
 
-import com.google.common.flogger.FluentLogger;
 import java.io.File;
 import java.io.Serializable;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
-public record Project(String name, String url, File folder, String sourceFolder)
-        implements Serializable, AutoCloseable {
-    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+public record Project(String name, String url, File folder, String sourceFolder) implements Serializable {
 
     public String getOwnerRepoName() {
         String[] split = StringUtils.split(url(), "/");
@@ -16,11 +12,5 @@ public record Project(String name, String url, File folder, String sourceFolder)
             return split[0] + "/" + split[1];
         }
         return split[split.length - 2] + "/" + split[split.length - 1];
-    }
-
-    @Override
-    public void close() throws Exception {
-        FileUtils.deleteDirectory(folder());
-        logger.atInfo().log("Cleaned %s", folder());
     }
 }
