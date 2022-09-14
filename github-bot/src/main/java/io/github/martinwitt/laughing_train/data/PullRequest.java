096ee6338124108040df94dcf497d4895bc1805e
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-09-14 16:39:27 +0200

    jandex

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/data/PullRequest.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/data/PullRequest.java
@@ -1,7 +1,10 @@
 package io.github.martinwitt.laughing_train.data;
 
+import java.io.Serializable;
+
 public record PullRequest(
-        PullRequestState state, String title, String body, String owner, String repo, int number, String url) {
+        PullRequestState state, String title, String body, String owner, String repo, int number, String url)
+        implements Serializable {
 
     public enum PullRequestState {
         OPEN,
