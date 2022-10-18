3731e88808ae345e8a67ecf28abe4a1c0dc777c3
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-10-18 19:24:34 +0200

    add new migration

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/persistence/DataBaseMigration.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/persistence/DataBaseMigration.java
@@ -1,7 +1,6 @@
 package io.github.martinwitt.laughing_train.persistence;
 
 import com.mongodb.client.model.Filters;
-import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 import io.quarkus.runtime.StartupEvent;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -15,8 +14,27 @@ public class DataBaseMigration {
      * This method is called by the quarkus framework to migrate the database.
      */
     public void onStart(@Observes StartupEvent event) {
-        BadSmell.mongoCollection()
-                .find(Filters.not(Filters.exists("position")))
-                .forEach(PanacheMongoEntityBase::delete);
+        removeBadSmellsWithoutPosition();
+        removeProjectHashesWithoutResults();
+    }
+
+    private void removeProjectHashesWithoutResults() {
+        for (Project project : Project.<Project>listAll()) {
+            boolean changed = false;
+            for (String hash : project.commitHashes) {
+                if (BadSmell.findByCommitHash(hash) == null
+                        || BadSmell.findByCommitHash(hash).isEmpty()) {
+                    project.commitHashes.remove(hash);
+                    changed = true;
+                }
+            }
+            if (changed) {
+                project.update();
+            }
+        }
+    }
+
+    private void removeBadSmellsWithoutPosition() {
+        BadSmell.mongoCollection().find(Filters.not(Filters.exists("position")));
     }
 }
