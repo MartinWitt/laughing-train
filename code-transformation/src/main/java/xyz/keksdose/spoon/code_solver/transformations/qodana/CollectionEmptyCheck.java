2c7713acaec193c194830697e7a596aba03018ba
MartinWitt <wittlinger.martin@gmail.com>
2022-10-24 18:33:49 +0000

    fix Bad Smells in multiple files

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/transformations/qodana/CollectionEmptyCheck.java
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/transformations/qodana/CollectionEmptyCheck.java
@@ -87,7 +87,7 @@ public class CollectionEmptyCheck extends TransformationProcessor<CtBinaryOperat
     private void notifyChangeListener(CtBinaryOperator<?> element, CtInvocation<Boolean> innvocation) {
         CtType<?> parent = element.getParent(CtType.class).getTopLevelType();
         String raw = "Replaced " + element + " with " + innvocation.toString();
-        String markdown = "Replaced `" + element + "` with `" + innvocation.toString() + "`";
+        String markdown = "Replaced `" + element + "` with `" + innvocation  + "`";
         setChanged(parent, new Change(COLLECTION_EMPTY_CHECK, MarkdownString.fromMarkdown(raw, markdown), parent));
     }
 
