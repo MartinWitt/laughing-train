358f9472dd5c27cf7254e3872b5dfd8e01a63d51
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-11-17 15:27:59 +0100

    fix: use event based mining

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
@@ -1,11 +1,17 @@
 package io.github.martinwitt.laughing_train.data;
 
+import com.google.common.flogger.FluentLogger;
+import java.io.Closeable;
 import java.io.File;
 import java.io.Serializable;
+import java.util.function.Supplier;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public record Project(String name, String url, File folder, String sourceFolder, String commitHash)
         implements Serializable {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
     public String getOwnerRepoName() {
         String[] split = StringUtils.split(url(), "/");
@@ -13,5 +19,20 @@ public record Project(String name, String url, File folder, String sourceFolder,
             return split[0] + "/" + split[1];
         }
         return split[split.length - 2] + "/" + split[split.length - 1];
+    }
+    /**
+     * Executes the given command in the project folder and deletes the folder afterwards
+     * @param runnable  the command to execute
+     */
+    public <T> T runInContext(Supplier<T> runnable) {
+        try (Closeable closeable = () -> FileUtils.deleteQuietly(folder())) {
+            return runnable.get();
+        } catch (Exception logged) {
+            // retry
+            if (!FileUtils.deleteQuietly(folder())) {
+                logger.atWarning().log("Could not delete folder %s", folder());
+            }
+        }
+        return null;
     }
 }
--- github-bot/src/main/java/io/github/martinwitt/laughing_train/mining/PeriodicMiner.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/mining/PeriodicMiner.java
@@ -8,26 +8,21 @@ import io.github.martinwitt.laughing_train.data.ProjectResult;
 import io.github.martinwitt.laughing_train.data.QodanaResult;
 import io.github.martinwitt.laughing_train.persistence.Project;
 import io.github.martinwitt.laughing_train.services.ServiceAdresses;
-import io.quarkus.scheduler.Scheduled;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Promise;
+import io.quarkus.runtime.StartupEvent;
+import io.smallrye.mutiny.Uni;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.DeliveryOptions;
-import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.eventbus.Message;
-import java.io.Closeable;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import io.vertx.mutiny.core.eventbus.Message;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.time.Duration;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import xyz.keksdose.spoon.code_solver.api.analyzer.AnalyzerResult;
@@ -46,99 +41,124 @@ public class PeriodicMiner {
     @Inject
     Vertx vertx;
 
-    private Project getRandomProject() {
-        return Project.<Project>mongoCollection()
-                .aggregate(List.of(Aggregates.sample(1)))
-                .first();
+    private Uni<Project> getRandomProject() {
+        return Uni.createFrom()
+                .item(Project.<Project>mongoCollection()
+                        .aggregate(List.of(Aggregates.sample(1)))
+                        .first());
     }
 
-    @Scheduled(every = "15m", delay = 3, delayUnit = TimeUnit.MINUTES)
-    void mineRandomRepo() {
-        Project project = getRandomProject();
-        if (project != null) {
-            eventBus.<ProjectResult>request(
-                    ServiceAdresses.PROJECT_REQUEST,
-                    new ProjectRequest.WithUrl(project.getProjectUrl()),
-                    new DeliveryOptions().setSendTimeout(TimeUnit.MINUTES.toMillis(300)),
-                    result -> vertx.executeBlocking(v -> mineProject(project.projectName, result)));
-        }
-    }
-
-    private Promise<Void> mineProject(String repoName, AsyncResult<Message<ProjectResult>> message) {
-        if (message.succeeded()) {
-            logger.atInfo().log("Mining periodic %s", repoName);
-            if (message.result().body() instanceof ProjectResult.Success project) {
-                List<Project> query =
-                        Project.findByProjectName(project.project().name());
-                if (query.size() == 1) {
-                    query.get(0).addCommitHash(project.project().commitHash());
-                    query.get(0).persistOrUpdate();
-                } else {
-                    var newProject = new Project(
-                            project.project().name(), project.project().url());
-                    newProject.addCommitHash(project.project().commitHash());
-                    newProject.persistOrUpdate();
-                }
-
-                eventBus.<QodanaResult>request(
-                        ServiceAdresses.QODANA_ANALYZER_REQUEST,
-                        new AnalyzerRequest.WithProject(project.project()),
-                        new DeliveryOptions().setSendTimeout(TimeUnit.MINUTES.toMillis(300)),
-                        result -> vertx.executeBlocking(v -> handle(result.result())));
-            } else {
-                logger.atWarning().log("Mining periodic %s failed", message);
+    void mine(@Observes StartupEvent event) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                mineRandomRepo();
             }
+        };
+        timer.schedule(task, TimeUnit.MINUTES.toMillis(5));
+    }
+
+    private void mineRandomRepo() {
+        getRandomProject()
+                .ifNoItem()
+                .after(Duration.ofSeconds(30))
+                .fail()
+                .onItem()
+                .transformToUni(this::checkoutProject)
+                .onItem()
+                .invoke(this::addOrUpdateCommitHash)
+                .onItem()
+                .transformToUni(this::analyzeProject)
+                .onItem()
+                .invoke(this::saveQodanaResults)
+                .subscribe()
+                .with(
+                        v -> {
+                            logger.atInfo().log("Finished mining. Next project starting now.");
+                            mineRandomRepo();
+                        },
+                        e -> {
+                            logger.atInfo().log("Failed mining" + e.getMessage());
+                            mineRandomRepo();
+                        });
+    }
+
+    private Uni<Message<ProjectResult>> checkoutProject(Project project) {
+        return eventBus.<ProjectResult>request(
+                ServiceAdresses.PROJECT_REQUEST,
+                new ProjectRequest.WithUrl(project.getProjectUrl()),
+                new DeliveryOptions().setSendTimeout(TimeUnit.MINUTES.toMillis(300)));
+    }
+
+    private Uni<Message<QodanaResult>> analyzeProject(Message<ProjectResult> message) {
+        if (message.body() instanceof ProjectResult.Success project) {
+            return eventBus.<QodanaResult>request(
+                    ServiceAdresses.QODANA_ANALYZER_REQUEST,
+                    new AnalyzerRequest.WithProject(project.project()),
+                    new DeliveryOptions().setSendTimeout(TimeUnit.MINUTES.toMillis(300)));
         } else {
-            logger.atSevere().log("Mining periodic %s failed with error %s", repoName, message.cause());
+            logger.atWarning().log("Mining periodic %s failed", message);
+            return Uni.createFrom().failure(new IllegalStateException("No project found"));
         }
-        return Promise.promise();
     }
 
-    private List<String> getRepoUrls(Path miningFile) throws IOException {
-        String repos =
-                StringUtils.substringBetween(Files.readString(miningFile), "<!-- repoStart -->", "<!-- reposEnd -->");
-        List<String> repoUrls = repos.lines().map(String::trim).collect(Collectors.toList());
-        repoUrls.removeIf(String::isEmpty);
-        return repoUrls;
-    }
-
-    private Git getWiki(Path dir) throws GitAPIException {
-        return Git.cloneRepository()
-                .setURI("https://github.com/MartinWitt/laughing-train.wiki.git")
-                .setDirectory(dir.toFile())
-                .call();
-    }
-
-    private Promise<Void> handle(Message<QodanaResult> message) {
-        if (message.body() instanceof QodanaResult.Success success) {
-            try (Closeable closeable =
-                    () -> FileUtils.deleteQuietly(success.project().folder())) {
-                List<AnalyzerResult> results = success.result();
-                if (results.isEmpty()) {
-                    logger.atInfo().log("No results for %s", success);
-                    return Promise.promise();
-                }
-                StringBuilder builder = new StringBuilder();
-                builder.append("# ")
-                        .append(success.project().getOwnerRepoName())
-                        .append(miningPrinter.printAllResults(results, success.project()));
-
-                var laughingRepo = GitHub.connectUsingOAuth(System.getenv("GITHUB_TOKEN"))
-                        .getRepository("MartinWitt/laughing-train");
-                updateOrCreateContent(laughingRepo, success.project().name(), builder.toString());
-            } catch (Exception e) {
-                logger.atSevere().withCause(e).log("Error while updating content");
+    private void addOrUpdateCommitHash(Message<ProjectResult> projectResult) {
+        if (projectResult.body() instanceof ProjectResult.Success project) {
+            String name = project.project().name();
+            List<Project> query = Project.findByProjectName(name);
+            String commitHash = project.project().commitHash();
+            if (hasSingleResult(query)) {
+                query.get(0).addCommitHash(commitHash);
+                query.get(0).persistOrUpdate();
+            } else {
+                String url = project.project().url();
+                var newProject = new Project(name, url);
+                newProject.addCommitHash(commitHash);
+                newProject.persistOrUpdate();
             }
         }
-        return Promise.promise();
+    }
+
+    private boolean hasSingleResult(List<Project> query) {
+        return query.size() == 1;
+    }
+
+    private Uni<Void> saveQodanaResults(Message<QodanaResult> message) {
+        if (message.body() instanceof QodanaResult.Success success) {
+            return success.project().runInContext(() -> {
+                try {
+                    List<AnalyzerResult> results = success.result();
+                    if (results.isEmpty()) {
+                        logger.atInfo().log("No results for %s", success);
+                        return Uni.createFrom().voidItem();
+                    }
+                    StringBuilder builder = new StringBuilder();
+                    builder.append("# ")
+                            .append(success.project().getOwnerRepoName())
+                            .append(miningPrinter.printAllResults(results, success.project()));
+
+                    var laughingRepo = getLaughingRepo();
+                    updateOrCreateContent(laughingRepo, success.project().name(), builder.toString());
+                } catch (Exception e) {
+                    logger.atSevere().withCause(e).log("Error while updating content");
+                }
+                return Uni.createFrom().voidItem();
+            });
+        }
+        return Uni.createFrom().voidItem();
+    }
+
+    private GHRepository getLaughingRepo() throws IOException {
+        return GitHub.connectUsingOAuth(System.getenv("GITHUB_TOKEN")).getRepository("MartinWitt/laughing-train");
     }
 
     private void updateOrCreateContent(GHRepository repo, String repoName, String content) {
         try {
             repo.getFileContent("mining/" + repoName + ".md", "gh-mining")
                     .update(content, "Update mining results for " + repoName, "gh-mining");
-        } catch (Exception logged) {
-            logger.atSevere().withCause(logged).log("Error while updating mining results");
+        } catch (Exception ignored) {
+            // this exception is thrown if the file does not exist yet for new projects
             try {
                 repo.createContent()
                         .content(content)
