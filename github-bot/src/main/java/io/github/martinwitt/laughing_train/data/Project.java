c0c21f574f7bd287148c1596c81cadc06973f89b
MartinWitt <wittlinger.martin@gmail.com>
2022-11-17 16:18:07 +0100

    ✨use event based mining (#289)
    This removes the delay between mining of repositories.

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/data/Project.java
@@ -1,11 +1,18 @@
 package io.github.martinwitt.laughing_train.data;
 
+import com.google.common.flogger.FluentLogger;
+import java.io.Closeable;
 import java.io.File;
 import java.io.Serializable;
+import java.util.Optional;
+import java.util.function.Supplier;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public record Project(String name, String url, File folder, String sourceFolder, String commitHash)
         implements Serializable {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
     public String getOwnerRepoName() {
         String[] split = StringUtils.split(url(), "/");
@@ -13,5 +20,20 @@ public record Project(String name, String url, File folder, String sourceFolder,
             return split[0] + "/" + split[1];
         }
         return split[split.length - 2] + "/" + split[split.length - 1];
+    }
+    /**
+     * Executes the given command in the project folder and deletes the folder afterwards
+     * @param runnable  the command to execute
+     */
+    public <T> Optional<T> runInContext(Supplier<? extends T> runnable) {
+        try (Closeable closeable = () -> FileUtils.deleteQuietly(folder())) {
+            return Optional.ofNullable(runnable.get());
+        } catch (Exception logged) {
+            // retry
+            if (!FileUtils.deleteQuietly(folder())) {
+                logger.atWarning().log("Could not delete folder %s", folder());
+            }
+        }
+        return Optional.empty();
     }
 }
