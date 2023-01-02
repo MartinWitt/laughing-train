02e314cb7bb7dc1e4b73c6345705e681411ca9eb
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-01-02 16:28:55 +0100

    ✨ Refactor rule RedundantArrayCreation

---
+++ code-transformation/src/test/java/xyz/keksdose/spoon/code_solver/transformations/qodana/RedundantArrayCreationTest.java
@@ -0,0 +1,22 @@
+package xyz.keksdose.spoon.code_solver.transformations.qodana;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import xyz.keksdose.spoon.code_solver.analyzer.qodana.rules.RedundantArrayCreation;
+import xyz.keksdose.spoon.code_solver.api.analyzer.AnalyzerResult;
+import xyz.keksdose.spoon.code_solver.api.analyzer.Position;
+import xyz.keksdose.spoon.code_solver.transformations.TestAnalyzerResult;
+import xyz.keksdose.spoon.code_solver.transformations.TransformationTestUtils;
+
+public class RedundantArrayCreationTest {
+    @Test
+    void spoonAnnotationFactory(@TempDir File dir) throws IOException {
+        Position position = new Position(114, 0, 90, 0, 3698, 12);
+        AnalyzerResult result = new TestAnalyzerResult("AnnotationFactory.java", position);
+        String resourcePath = "projects/refactorings/RedundantArrayCreation/AnnotationFactory.java";
+        var copy = TransformationTestUtils.transform(new RedundantArrayCreation(result), resourcePath, dir);
+        TransformationTestUtils.compareContent(copy, resourcePath);
+    }
+}
