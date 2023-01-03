5e393661acb350da80a1f10ac2a6686c5647b5f3
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-01-03 21:50:30 +0100

    🚑 Fix master CI

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/api/graphql/dto/ProjectConfigGraphQLDtoInput.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/api/graphql/dto/ProjectConfigGraphQLDtoInput.java
@@ -2,9 +2,35 @@ package io.github.martinwitt.laughing_train.api.graphql.dto;
 
 import org.eclipse.microprofile.graphql.Name;
 
+import io.github.martinwitt.laughing_train.domain.entity.ProjectConfig;
+
 @Name("ProjectConfig")
-public class ProjectConfigGraphQLDtoInput extends ProjectConfigGraphQLDto {
+public class ProjectConfigGraphQLDtoInput {
+
+    private String sourceFolder;
+    private String projectUrl;
 
     @SuppressWarnings("NullAway")
     public ProjectConfigGraphQLDtoInput() {}
+
+    public ProjectConfigGraphQLDtoInput(ProjectConfig projectConfig) {
+        this.sourceFolder = projectConfig.getSourceFolder();
+        this.projectUrl = projectConfig.getProjectUrl();
+    }
+
+    public String getSourceFolder() {
+        return this.sourceFolder;
+    }
+
+    public void setSourceFolder(String sourceFolder) {
+        this.sourceFolder = sourceFolder;
+    }
+
+    public String getProjectUrl() {
+        return this.projectUrl;
+    }
+
+    public void setProjectUrl(String projectUrl) {
+        this.projectUrl = projectUrl;
+    }
 }
