2e57c769972c0c51319522aa16819590820843d5
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-01-07 16:52:54 +0100

    💄 Fix add all in refactor view

--- frontend/src/pages/RefactorView.tsx
+++ frontend/src/pages/RefactorView.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { Alert, Button, Chip, Stack, TextField, Typography } from "@mui/material";
-import Grid2  from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 import React, { useMemo } from "react";
 import { useParams } from "react-router";
@@ -11,17 +11,20 @@ import { fetchBadSmellsforHashQuery, refactorQuery } from "../ProjectData";
 
 
 export function RefactorView() {
-  const [selected, addSelected] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState<string[]>([]);
   const [search, setSearch] = React.useState<string>("");
-  const handleSelect = (value: string) => {
-    if (selected.includes(value)) {
-      addSelected(selected.filter((identifier: string) => identifier !== value));
-      console.log("remove" + value);
-      console.log(selected);
-    } else {
-      addSelected([...selected, value]);
-      console.log("added " + value);
-      console.log([...selected, value]);
+  const handleSelect = (values: string[]) => {
+
+    if (values.length === 1) {
+      if (selected.includes(values[0])) {
+        setSelected(selected.filter((identifier: string) => identifier !== values[0]));
+      } else {
+        setSelected([...selected, values[0]]);
+      }
+    }
+    else {
+      const temp = [...selected, ...values];
+      setSelected(temp);
     }
   };
   const [refactorBadSmell] = useMutation(
@@ -33,15 +36,19 @@ export function RefactorView() {
       }
     }
   )
-  // const { data : rules, loading : loadingRules, error : errorRules } = useQuery(fetchAvailableRefactorings);
+  // function combine 2 arrays:
+  // const arr1 = [1, 2, 3];
+  // const arr2 = [4, 5, 6];
+  // const arr3 = [...arr1, ...arr2];
+
   const { hash, name } = useParams();
-  const { data : dataBadSmell, error : errorBadSmell, loading : loadingBadSmell } = useQuery(fetchBadSmellsforHashQuery, {
+  const { data: dataBadSmell, error: errorBadSmell, loading: loadingBadSmell } = useQuery(fetchBadSmellsforHashQuery, {
     variables: {
       hash: hash
     }
   });
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  let allBadsmells: BadSmell[] = (dataBadSmell !==undefined) ? dataBadSmell.byCommitHash : [];
+  let allBadsmells: BadSmell[] = (dataBadSmell !== undefined) ? dataBadSmell.byCommitHash : [];
   allBadsmells = useMemo(() =>
     allBadsmells.filter((badSmell: BadSmell) => {
       return badSmell.ruleID.toLowerCase().includes(search.toLowerCase()) || badSmell.messageMarkdown.toLowerCase().includes(search.toLowerCase());
@@ -53,14 +60,14 @@ export function RefactorView() {
       <Typography variant="h4" alignContent={"center"}>Hash not found. Please go back an select a hash before</Typography>
     </div>;
   }
-  if (/*errorRules ||*/ errorBadSmell) {
+  if (errorBadSmell) {
     return <Alert severity="error">Error Loading ruleset. Are you logged in?</Alert>
   }
-  if (/* loadingRules ||*/ loadingBadSmell) {
+  if (loadingBadSmell) {
     return <Alert severity="info">Loading</Alert>;
   }
 
-    return (
+  return (
 
     <div>
       <Headline />
@@ -70,46 +77,51 @@ export function RefactorView() {
         <Grid2 xs={2} >
           <Typography variant="h4"> Search</Typography>
           <br />
-            <TextField label="Filter" value={search} onChange={(e) => setSearch(e.target.value)} />
-            <br />
-            <StackSelector {...{ dataBadSmell, handleSelect }} />
+          <TextField label="Filter" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <br />
+          <StackSelector {...{ dataBadSmell, handleSelect }} />
         </Grid2>
         <Grid2 xs={8}><BadSmellTreeView {...{ badSmell: allBadsmells, selector: handleSelect }} /></Grid2>
-          <Grid2 xs={2}>
+        <Grid2 xs={2}>
           <StackSelectedRule {...{ selected: selected, handleSelect: handleSelect }} />
-          <Button variant="contained" size="large" value={""} onClick={(e) => {
-            e.preventDefault();
+          <Button variant="contained" size="large" value={""} onClick={() => {
             refactorBadSmell({
               variables: {
                 badSmellIdentifier: selected,
               }
             });
-            }} >Refactor</Button>
-          </Grid2>
+          }} >Refactor</Button>
+        </Grid2>
       </Grid2>
-        </div>
-    );
+    </div>
+  );
 }
-function StackSelectedRule({ selected, handleSelect }: { selected: string[]; handleSelect: (value: string) => void; }) {
-  console.log(selected);
+function StackSelectedRule({ selected, handleSelect }: { selected: string[]; handleSelect: (value: [string]) => void; }) {
   return <Stack>
-    {selected.map((value) => {
-      return <Chip  label={value} key={value} size="medium" onClick={() => handleSelect(value)} />;
+    {selected.map((value : string) => {
+      return <Chip label={value} key={value} size="medium" onClick={() => handleSelect([value])} />;
     })}
   </Stack>;
 }
 
-function StackSelector({ dataBadSmell, handleSelect }: { dataBadSmell: any; handleSelect: (value: string) => void; }) {
+function StackSelector({ dataBadSmell, handleSelect }: { dataBadSmell: any; handleSelect: (value: string[]) => void; }): JSX.Element {
+  const badSmells: BadSmell[] = dataBadSmell.byCommitHash;
   return <Stack spacing={0.5}>
-    {listOfUniqueRules(dataBadSmell.byCommitHash).map((badSmell: BadSmell) => {
-      return <Chip  key={badSmell.ruleID} label={badSmell.ruleID} onClick={(e) => dataBadSmell.byCommitHash.filter((badSmell: BadSmell) => {
-        console.log(e.currentTarget.innerText)
-        return badSmell.ruleID === e.currentTarget.innerText;
-      }).forEach((badSmell: BadSmell) => {
-        handleSelect(badSmell.identifier);
-      })} />;
+    {listOfUniqueRules(badSmells).map((badSmell: BadSmell) => {
+      return <Chip key={badSmell.ruleID} label={badSmell.ruleID} onClick={(e) => handleSelect(toRuleIDs(filterBadSmells(badSmells, e.currentTarget.innerText)))} />;
     })}
   </Stack>;
+}
+
+function filterBadSmells(BadSmell : BadSmell[], ruleID : string) : BadSmell[]{
+  return BadSmell.filter((inner: BadSmell) => {
+    return inner.ruleID === ruleID;
+  });
+}
+function toRuleIDs(badSmells: BadSmell[]): string[] {
+  return badSmells.map((badSmell: BadSmell) => {
+    return badSmell.identifier;
+  });
 }
 
 function listOfUniqueRules(badSmells: BadSmell[]): BadSmell[] {
