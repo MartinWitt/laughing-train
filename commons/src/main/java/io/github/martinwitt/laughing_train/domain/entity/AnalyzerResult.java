7872cf4313b75e767e1a75cabceb91b701e8ed23
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-03-18 15:55:42 +0100

    fix spotless

--- commons/src/main/java/io/github/martinwitt/laughing_train/domain/entity/AnalyzerResult.java
+++ commons/src/main/java/io/github/martinwitt/laughing_train/domain/entity/AnalyzerResult.java
@@ -3,7 +3,6 @@ package io.github.martinwitt.laughing_train.domain.entity;
 import io.github.martinwitt.laughing_train.domain.value.Position;
 import io.github.martinwitt.laughing_train.domain.value.RuleId;
 import java.io.Serializable;
-import javax.annotation.Nonnull;
 
 /**
  * This represents a single issue of an analyzer run. It contains the position of the issue in the source file, the message and the message in markdown format.
@@ -12,7 +11,6 @@ public interface AnalyzerResult extends Serializable {
 
     String getAnalyzer();
 
-    @Nonnull
     RuleId ruleID();
 
     String filePath();
