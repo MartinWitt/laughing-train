4521edede940ea228423efb6f91485d28cc23580
MartinWitt <wittlinger.martin@gmail.com>
2023-03-19 23:45:47 +0100

    🐛 Fix handling of line endings at end of file (#511)

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/diffs/DiffCleaner.java
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/diffs/DiffCleaner.java
@@ -6,6 +6,7 @@ import com.google.common.flogger.FluentLogger;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -14,9 +15,14 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import xyz.keksdose.spoon.code_solver.history.Change;
 
 /**
@@ -41,34 +47,41 @@ public class DiffCleaner {
         try (Git git = Git.open(path.toFile());
                 ObjectReader reader = git.getRepository().newObjectReader();
                 DiffFormatter df = new DiffFormatter(out)) {
-            CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
-            oldTreeIter.reset(reader, getLastCommit(git));
+            // get the path to the file in the repository and the line endings
             String shortPath = getRelativeRepoPath(change, git);
             String lineEnding = detectLineSeparator(Files.readString(
                     change.getAffectedType().getPosition().getFile().toPath()));
+            boolean hasLineEnding = fileHasLineEnding(git, shortPath);
             Path filePath = path.resolve(shortPath);
+
+            // prepare the two iterators to compute the diff between
+            CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+            oldTreeIter.reset(reader, getLastCommit(git));
             CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
             var fakeCommit = createFakeCommit(git, shortPath);
             newTreeIter.reset(reader, fakeCommit);
+
             // finally get the list of changed files
             List<DiffEntry> diffs =
                     git.diff().setNewTree(newTreeIter).setOldTree(oldTreeIter).call();
             setDiffFormaterSettings(git, df);
             createDiffs(df, diffs);
-            var list = out.toString().lines().toList();
-            PeekingIterator<String> it = Iterators.peekingIterator(list.iterator());
+            PeekingIterator<String> it =
+                    Iterators.peekingIterator(out.toString().lines().toList().iterator());
+
             while (it.hasNext()) {
-                var line = it.next();
-                if (line.startsWith("- ") && !line.startsWith("---")) {
+                String line = it.next();
+                if (isDeletedLines(line)) {
                     var next = it.peek();
-                    if (next.startsWith("+ ")) {
+                    if (isAddedLine(next)) {
                         GitLineChange lineChange = new GitLineChange(
-                                list.indexOf(next.substring(2)), line.substring(1), next.substring(1));
+                                out.toString().lines().toList().indexOf(next.substring(2)),
+                                line.substring(1),
+                                next.substring(1));
                         if (change.getModes().contains(DiffCleanModes.NO_WHITESPACE_ADD)) {
-                            Files.writeString(
-                                    filePath,
-                                    new ExtraWhiteSpaceCleaner()
-                                            .clean(Files.readString(filePath), lineChange, change, lineEnding));
+                            String cleanResult = new ExtraWhiteSpaceCleaner()
+                                    .clean(Files.readString(filePath), lineChange, change, lineEnding);
+                            printResult(lineEnding, hasLineEnding, filePath, cleanResult);
                         }
                     }
                 }
@@ -76,6 +89,23 @@ public class DiffCleaner {
             git.reset().setRef("HEAD~1").call();
         } catch (Exception e) {
             logger.atSevere().withCause(e).log("could not clean because not a git repo");
+        }
+    }
+
+    private boolean isAddedLine(String next) {
+        return next.startsWith("+ ");
+    }
+
+    private boolean isDeletedLines(String line) {
+        return line.startsWith("- ") && !line.startsWith("---");
+    }
+
+    private void printResult(String lineEnding, boolean hasLineEnding, Path filePath, String cleanResult)
+            throws IOException {
+        if (hasLineEnding) {
+            Files.writeString(filePath, cleanResult + lineEnding);
+        } else {
+            Files.writeString(filePath, cleanResult);
         }
     }
 
@@ -140,5 +170,38 @@ public class DiffCleaner {
             }
         }
         return System.getProperty("line.separator");
+    }
+    /**
+     * Gets the content of a file at a specific commit.
+     * @param git   the git repository
+     * @param commit  the commit to get the content from
+     * @param path  the path to the file
+     * @return  the content of the file
+     * @throws IOException  if the file could not be read
+     */
+    private String getContent(Git git, RevCommit commit, String path) throws IOException {
+        try (TreeWalk treeWalk = TreeWalk.forPath(git.getRepository(), path, commit.getTree())) {
+            ObjectId blobId = treeWalk.getObjectId(0);
+            try (ObjectReader objectReader = git.getRepository().newObjectReader()) {
+                ObjectLoader objectLoader = objectReader.open(blobId);
+                byte[] bytes = objectLoader.getBytes();
+                return new String(bytes, StandardCharsets.UTF_8);
+            }
+        }
+    }
+    /**
+     * Checks if the file has a line ending at the end of the file.
+     * @param git   the git repository
+     * @param filePath  the path to the file
+     * @return  true if the file has a line ending at the end of the file
+     */
+    private boolean fileHasLineEnding(Git git, String filePath) {
+        try (RevWalk revWalk = new RevWalk(git.getRepository())) {
+            RevCommit commit = revWalk.parseCommit(git.getRepository().resolve("HEAD~1"));
+            return getContent(git, commit, filePath).endsWith("\n");
+        } catch (IOException e) {
+            logger.atSevere().withCause(e).log("could not get content");
+            return false;
+        }
     }
 }
