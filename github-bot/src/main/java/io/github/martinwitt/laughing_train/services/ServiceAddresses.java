087d7a48e1dd2b98cc6de0156fca9bc2d7f99ded
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-05-07 20:59:36 +0200

    ♻️ Improve project service usage (#626)

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/services/ServiceAddresses.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/services/ServiceAddresses.java
@@ -4,7 +4,6 @@ public class ServiceAddresses {
     private ServiceAddresses() {}
 
     public static final String QODANA_ANALYZER_RESPONSE = "qodana.analyzer.response";
-    public static final String PROJECT_REQUEST = "project.request";
     public static final String PROJECT_RESPONSE = "project.response";
     public static final String FIND_ISSUE_REQUEST = "github.issue.request";
     public static final String FIND_SUMMARY_ISSUE_REQUEST = "github.issue-summary.request";
