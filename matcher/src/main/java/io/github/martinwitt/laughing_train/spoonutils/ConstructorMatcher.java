43a84830b4f6b6f958e5bc13478f0f5daf95c6be
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-07-08 22:51:02 +0200

    feat(matcher): Add new matchers for spoon elements (#804)

---
+++ matcher/src/main/java/io/github/martinwitt/laughing_train/spoonutils/ConstructorMatcher.java
@@ -0,0 +1,60 @@
+package io.github.martinwitt.laughing_train.spoonutils;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+import spoon.reflect.code.CtConstructorCall;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.Filter;
+
+/**
+ * A filter for matching constructor calls with a specific target type and argument types.
+ */
+public class ConstructorMatcher implements Filter<CtConstructorCall<?>> {
+
+    private final String fqTargetType;
+    private final String[] argsFQN;
+
+    /**
+     * Creates a new constructor matcher with the given target type and argument types.
+     *
+     * @param fqTargetType the fully-qualified name of the target type
+     * @param argsFQN      the fully-qualified names of the argument types
+     */
+    public ConstructorMatcher(String fqTargetType, String... argsFQN) {
+        this.fqTargetType = fqTargetType;
+        this.argsFQN = argsFQN;
+    }
+
+    /**
+     * Determines whether the given constructor call matches the target type and argument types.
+     *
+     * @param element the constructor call to match
+     * @return true if the constructor call matches the target type and argument types, false otherwise
+     */
+    @Override
+    public boolean matches(CtConstructorCall<?> element) {
+        if (element == null) {
+            return false;
+        }
+
+        if (!element.getType().getQualifiedName().equals(fqTargetType)) {
+            return false;
+        }
+        if (argsFQN == null || argsFQN.length == 0) {
+            return true;
+        }
+        if (element.getArguments().size() != argsFQN.length) {
+            return false;
+        }
+
+        List<Pair<CtTypeReference<?>, CtExpression<?>>> zipped = new ArrayList<>();
+        for (int i = 0; i < argsFQN.length && i < element.getArguments().size(); i++) {
+            zipped.add(Pair.of(
+                    element.getFactory().createReference(argsFQN[i]),
+                    element.getArguments().get(i)));
+        }
+        return zipped.stream().allMatch(pair -> pair.getRight().getType().isSubtypeOf(pair.getLeft()));
+    }
+}
---
+++ matcher/src/main/java/io/github/martinwitt/laughing_train/spoonutils/InvocationMatcher.java
@@ -0,0 +1,82 @@
+package io.github.martinwitt.laughing_train.spoonutils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.apache.commons.lang3.tuple.Pair;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtTypeAccess;
+import spoon.reflect.reference.CtTypeReference;
+import spoon.reflect.visitor.Filter;
+
+/**
+ * A matcher that checks if a given {@link CtInvocation} object matches a specified target type, method name, and argument types.
+ */
+public class InvocationMatcher implements Filter<CtInvocation<?>> {
+    private final String fqTargetType;
+    private final String methodName;
+    private final String[] argsFQN;
+
+    /**
+     * Creates a new {@link InvocationMatcher} object with the specified target type, method name, and argument types.
+     * @param fqTargetType the fully qualified name of the target type
+     * @param methodName the name of the method
+     * @param argsFQN the fully qualified names of the argument types
+     */
+    public InvocationMatcher(String fqTargetType, String methodName, String... argsFQN) {
+        this.fqTargetType = fqTargetType;
+        this.methodName = methodName;
+        this.argsFQN = argsFQN;
+    }
+
+    /**
+     * Checks if the specified {@link CtInvocation} object matches the target type, method name, and argument types of this {@link InvocationMatcher}.
+     * @param element the {@link CtInvocation} object to check
+     * @return true if the invocation matches, false otherwise
+     */
+    public boolean matches(CtInvocation<?> element) {
+        if (element == null) {
+            return false;
+        }
+
+        // Check if the target type matches
+        CtExpression<?> target = element.getTarget();
+        if (target == null || target.getType() == null) {
+            return false;
+        }
+        if (target instanceof CtTypeAccess access) {
+            if (!access.getAccessedType().getQualifiedName().equals(fqTargetType)) {
+                return false;
+            }
+        } else {
+            if (!target.getType().getQualifiedName().equals(fqTargetType)) {
+                return false;
+            }
+        }
+
+        // Check if the method name matches
+        if (Optional.ofNullable(element.getExecutable())
+                .map(v -> v.getExecutableDeclaration())
+                .filter(v -> v.getSimpleName().equals(methodName))
+                .isEmpty()) {
+            return false;
+        }
+
+        // Check if the argument types match
+        if (argsFQN == null || argsFQN.length == 0) {
+            return true;
+        }
+        if (element.getArguments().size() != argsFQN.length) {
+            return false;
+        }
+
+        List<Pair<CtTypeReference<?>, CtExpression<?>>> zipped = new ArrayList<>();
+        for (int i = 0; i < argsFQN.length && i < element.getArguments().size(); i++) {
+            zipped.add(Pair.of(
+                    element.getFactory().createReference(argsFQN[i]),
+                    element.getArguments().get(i)));
+        }
+        return zipped.stream().allMatch(pair -> pair.getRight().getType().isSubtypeOf(pair.getLeft()));
+    }
+}
