8ff0701ed613630fb2434297096a318eab1144a2
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-07-10 23:53:16 +0200

    fix(vertx): Use timeunits for max blocking time

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/mining/QodanaPeriodicMiner.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/mining/QodanaPeriodicMiner.java
@@ -81,7 +81,8 @@ public class QodanaPeriodicMiner {
         try {
             logger.atInfo().log("Starting Qodana periodic miner");
             vertx.exceptionHandler(it -> logger.atWarning().withCause(it).log("Exception in vertx"));
-            vertx.setTimer(TimeUnit.MINUTES.toMillis(3), v -> vertx.createSharedWorkerExecutor("MINING", 5, 30L)
+            vertx.setTimer(TimeUnit.MINUTES.toMillis(3), v -> vertx.createSharedWorkerExecutor(
+                            "MINING", 5, 30L, TimeUnit.MINUTES)
                     .executeBlocking(it -> mineRandomRepo()));
         } catch (Exception e) {
             logger.atWarning().withCause(e).log("Failed to repo with Qodana");
@@ -126,7 +127,8 @@ public class QodanaPeriodicMiner {
         } finally {
             logger.atInfo().log("Queue size: %s", queue.size());
             logger.atInfo().log("Mining next repo in 1 minute");
-            vertx.setTimer(TimeUnit.MINUTES.toMillis(1), v -> vertx.createSharedWorkerExecutor("MINING", 5, 30L)
+            vertx.setTimer(TimeUnit.MINUTES.toMillis(1), v -> vertx.createSharedWorkerExecutor(
+                            "MINING", 5, 30L, TimeUnit.MINUTES)
                     .executeBlocking(it -> mineRandomRepo()));
         }
     }
