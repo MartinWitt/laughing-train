e2f53e5ba268e1352184a5eb9343e76e9b89a09b
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-07-16 00:16:40 +0200

    fix(spoon): Improve error logging to contain badsmell classname

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/analyzer/spoon/AnalyzerResultVisitor.java
+++ code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/analyzer/spoon/AnalyzerResultVisitor.java
@@ -39,7 +39,7 @@ class AnalyzerResultVisitor implements BadSmellVisitor<AnalyzerResult> {
 
         } catch (Exception e) {
             logger.atWarning().withCause(e).withStackTrace(StackSize.NONE).log(
-                    "Could not convert bad smell to analyzer result");
+                    "Could not convert bad smell to analyzer result %s", badSmell.getClass());
             return Optional.empty();
         }
     }
