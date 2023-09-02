23d8a59eaf245555a894567dd261b5464ffeb631
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-09-02 15:52:33 +0200

    refactor(spoon): 🔥 Delete old API from code transformation

--- code-transformation/src/main/java/xyz/keksdose/spoon/code_solver/api/AfterRefactorStep.java
+++
@@ -1,9 +0,0 @@
-package xyz.keksdose.spoon.code_solver.api;
-
-import java.io.File;
-import xyz.keksdose.spoon.code_solver.history.Changelog;
-
-public interface AfterRefactorStep {
-
-    void apply(Changelog changelog, File projectRoot);
-}
