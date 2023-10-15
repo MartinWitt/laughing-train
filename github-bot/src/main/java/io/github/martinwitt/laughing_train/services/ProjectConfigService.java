67fb564aedf90c40c0593ae4e26348efd0fd29a3
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-10-15 19:09:27 +0200

    refactor: cleanup gitignore file removing duplicate entries

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/services/ProjectConfigService.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/services/ProjectConfigService.java
@@ -4,7 +4,6 @@ import com.google.common.flogger.FluentLogger;
 import io.github.martinwitt.laughing_train.data.FindProjectConfigRequest;
 import io.github.martinwitt.laughing_train.domain.entity.ProjectConfig;
 import io.github.martinwitt.laughing_train.persistence.repository.ProjectConfigRepository;
-import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.List;
@@ -14,15 +13,6 @@ public class ProjectConfigService {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   @Inject ProjectConfigRepository projectConfigRepository;
-
-  public Uni<List<ProjectConfig>> getConfig(FindProjectConfigRequest request) {
-    if (request instanceof FindProjectConfigRequest.ByProjectUrl byProjectUrl) {
-      return Uni.createFrom()
-          .item(projectConfigRepository.findByProjectUrl(byProjectUrl.projectUrl()));
-    }
-    logger.atWarning().log("Unknown request type %s", request.getClass());
-    return Uni.createFrom().failure(new IllegalArgumentException("Unknown request type"));
-  }
 
   public List<ProjectConfig> getProjectConfig(FindProjectConfigRequest request) {
     if (request instanceof FindProjectConfigRequest.ByProjectUrl byProjectUrl) {
