332f62c940ce40bfa5a1316fd7d1a5525a2a9bd2
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-10-20 14:29:54 -0400

    refactor: Rename service with a more fitting name

--- github-bot/src/main/java/io/github/martinwitt/laughing_train/services/GitHubIssueSearch.java
+++ github-bot/src/main/java/io/github/martinwitt/laughing_train/services/GitHubIssueSearch.java
@@ -2,6 +2,8 @@ package io.github.martinwitt.laughing_train.services;
 
 import com.google.common.flogger.FluentLogger;
 import io.github.martinwitt.laughing_train.data.FindIssueRequest;
+import io.github.martinwitt.laughing_train.data.FindIssueRequest.WithUserName;
+import io.github.martinwitt.laughing_train.data.FindIssueRequest.WithUserNameAndRepoAndTitle;
 import io.github.martinwitt.laughing_train.data.FindIssueResult;
 import io.github.martinwitt.laughing_train.data.FindPullRequestResult;
 import io.github.martinwitt.laughing_train.data.GitHubState;
@@ -16,23 +18,23 @@ import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GitHub;
 
 @ApplicationScoped
-public class IssueRequestService {
+public class GitHubIssueSearch {
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   public FindPullRequestResult findPullRequests(FindIssueRequest request) {
     logger.atFine().log("Got request %s", request);
-    if (request instanceof FindIssueRequest.WithUserName userName) {
-      logger.atFine().log("Got user name %s", userName);
-      return getIssuesWithFixes(userName);
-    }
-    throw new IllegalArgumentException("Unknown request type %s".formatted(request));
+    return switch (request) {
+      case WithUserName userName -> getIssuesWithFixes(userName);
+      case WithUserNameAndRepoAndTitle __ -> throw new IllegalArgumentException(
+          "Not implemented yet");
+    };
   }
 
   private FindPullRequestResult getIssuesWithFixes(FindIssueRequest.WithUserName userName) {
     try {
       return new FindPullRequestResult.MultipleResults(
-          convertGHIssueToPullRequest(searchPrs(userName)));
+          convertGHIssueToPullRequest(searchPullRequests(userName)));
     } catch (IOException e) {
       logger.atSevere().withCause(e).log(
           "Error while searching for issues with fixes for user %s", userName);
@@ -44,7 +46,8 @@ public class IssueRequestService {
     return issues.stream().map(this::toPullRequest).collect(Collectors.toList());
   }
 
-  private List<GHIssue> searchPrs(FindIssueRequest.WithUserName userName) throws IOException {
+  private List<GHIssue> searchPullRequests(FindIssueRequest.WithUserName userName)
+      throws IOException {
     return GitHub.connectUsingOAuth(System.getenv("GITHUB_TOKEN"))
         .searchIssues()
         .q("is:pr")
@@ -78,26 +81,25 @@ public class IssueRequestService {
 
   private FindIssueResult findSummaryIssue() {
     try {
-      var list = searchSummaryIssueOnGithub();
-      if (list.isEmpty()) {
-        return new FindIssueResult.NoResult();
-      }
-      return new FindIssueResult.MultipleResults(list.stream().map(this::toIssue).toList());
-    } catch (IOException e) {
+      GHIssue issue = searchSummaryIssueOnGithub();
+      return new FindIssueResult.SingleResult(toIssue(issue));
+    } catch (Exception e) {
       logger.atSevere().withCause(e).log("Error while searching for summary issue");
       return new FindIssueResult.NoResult();
     }
   }
 
-  private List<GHIssue> searchSummaryIssueOnGithub() throws IOException {
+  private GHIssue searchSummaryIssueOnGithub() throws IOException {
     return GitHub.connectUsingOAuth(System.getenv("GITHUB_TOKEN"))
         .getRepository("MartinWitt/laughing-train")
         .queryIssues()
         .pageSize(100)
         .label("laughing-train-summary")
         .state(GHIssueState.OPEN)
+        .pageSize(1)
         .list()
-        .toList();
+        .toList()
+        .getFirst();
   }
 
   private Issue toIssue(GHIssue issue) {
