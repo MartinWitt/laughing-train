ebc130b38f61c7a50e9ec524ac5bad72fc0b44ef
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-12-24 16:23:44 +0100

    up

--- frontend/src/component/Headline.tsx
+++ frontend/src/component/Headline.tsx
@@ -10,7 +10,7 @@ class Headline extends React.Component {
         <Toolbar>
           {buttons(pages)}
           <div />
-          <LoginButton />
+          <LoginButton sx={{}} />
         </Toolbar>
       </AppBar>
     );
