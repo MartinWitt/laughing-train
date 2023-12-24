323e96909c709f07c7d0aa265423de499a5c7fa9
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-12-24 15:03:37 +0100

    feat: Add Sidebar component and update dependencies
    Introduced new Sidebar component and simplified PageLayout component by using the new Sidebar instead of Navigation component. Added new dependencies (@react-buddy/ide-toolbox and @react-buddy/palette-mui) in package.json and updated package-lock.json accordingly. The LoginButton component was also updated to accept styling props.

---
+++ frontend/src/component/Sidebar.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import { LoginButton } from './LoginButton';
+import { ListItemButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+export type LinkType = { title: string; url: string };
+
+type Props = { links: LinkType[] };
+interface ListItemsProps {
+  links: LinkType[];
+}
+
+function ListItems(props: ListItemsProps) {
+  const { links } = props;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {links.map(({ title, url }) => (
+        <ListItemButton key={url}>
+          <ListItemIcon>
+            <MenuIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={title}
+            sx={{ color: '#fff' }}
+            onClick={() => navigate(url)}
+          />
+        </ListItemButton>
+      ))}
+    </>
+  );
+}
+
+function Sidebar(props: Props) {
+  const { links } = props;
+  return (
+    <Box sx={{ width: 250, bgcolor: '#272727', height: '100vh' }}>
+      <Typography
+        variant="h6"
+        noWrap
+        component="div"
+        align="center"
+        sx={{ flexGrow: 1, p: 2 }}
+      >
+        Laughing-Train
+      </Typography>
+      <Divider />
+      <List>
+        <ListItems links={props.links} />
+      </List>
+      <LoginButton
+        sx={{ mx: 1, my: 1, color: '#fff', borderColor: '#fff', width: '90%' }}
+      />
+    </Box>
+  );
+}
+
+export default Sidebar;
--- frontend/src/pages/PageLayout.tsx
+++ frontend/src/pages/PageLayout.tsx
@@ -11,53 +11,24 @@ import {
 } from '@mui/material';
 import { LoginButton } from '../component/LoginButton';
 import { useNavigate } from 'react-router';
+import Sidebar, { LinkType } from '../component/Sidebar';
 
 interface NameProps {
   name: string;
 }
 
-function Name({ name }: NameProps) {
-  return (
-    <h1 style={{ fontSize: '20px', fontWeight: 'bold', margin: '0 0 0 5px' }}>
-      {name}
-    </h1>
-  );
-}
-
-function Navigation({ links }: NavigationProps) {
-  const navigate = useNavigate();
-  return (
-    <List>
-      {links.map((link) => (
-        <>
-          <ListItem
-            key={link.name}
-            onClick={() => navigate(link.href, { replace: true })}
-          >
-            <ListItemText primary={link.name} />
-          </ListItem>
-          <Divider sx={{ height: 1, backgroundColor: 'white' }} />
-        </>
-      ))}
-      <LoginButton />
-    </List>
-  );
-}
-interface PageLayoutProps {
-  children: React.ReactNode;
-}
 interface Link {
   name: string;
   href: string;
 }
-interface NavigationProps {
-  links: Link[];
+interface PageLayoutProps {
+  children: React.ReactNode;
 }
 
 export default function PageLayout({ children }: PageLayoutProps) {
-  const navigationItems: Link[] = [
-    { name: 'Home', href: '/' },
-    { name: 'Statistics', href: '/statistics' },
+  const navigationItems: LinkType[] = [
+    { title: 'Home', url: '/' },
+    { title: 'Statistics', url: '/statistics' },
   ];
   return (
     <div style={{ height: '100vh' }}>
@@ -67,25 +38,7 @@ export default function PageLayout({ children }: PageLayoutProps) {
         style={{ height: 'calc(100%) ', flexWrap: 'nowrap' }}
       >
         <Grid item sx={{ width: '200px' }}>
-          <Box
-            sx={{
-              height: '100%',
-              bgcolor: '#272727',
-              overflow: 'hidden',
-              position: 'fixed',
-            }}
-          >
-            <AppBar
-              position="static"
-              elevation={0}
-              sx={{ bgcolor: '#272727', marginTop: '10px', marginLeft: '10px' }}
-            >
-              <Name name="Laughing-Train" />
-              <Toolbar>
-                <Navigation links={navigationItems} />
-              </Toolbar>
-            </AppBar>
-          </Box>
+          <Sidebar links={navigationItems} />
         </Grid>
         <Grid item xs={9} sx={{}}>
           {children}
