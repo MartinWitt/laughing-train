3b716b9d68fedbb2f1fb63d5096a8f666fc380cd
Martin Wittlinger <wittlinger.martin@gmail.com>
2024-01-11 11:55:10 +0100

    Refactor components and add error/loading states in Dashboard page
    Various parts of the project have been refactored for better readability and organization. The DashBoardItem component now uses props for better clarity, and a console logging line was removed from ResultView component. Additionally, loading and error states were added to the DashBoard page to enhance user experience.

--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -1,30 +1,71 @@
 import { Box, Button, Grid, TextField, Typography } from '@mui/material';
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { useNavigate } from 'react-router';
+import { useGetProjectsQuery } from '../gql/graphql-types';
+import AppBreadcrumbs from '../component/AppBreadcrumbs';
+import { AppErrorMessage } from '../component/AppErrorMessage';
+import { AppLoadingBar } from '../component/AppLoadingBar';
+import { Project } from '../data/Project';
+import DashBoardItem from '../component/DashBoardItem';
+
+const breadcrumbItems = [
+  { text: 'Home', href: '/home' },
+  { text: 'Dashboard' },
+];
+
+interface SearchFieldProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+function SearchField(props: SearchFieldProps) {
+  return (
+    <>
+      <Typography variant="h4">Search</Typography>
+      <TextField
+        value={props.value}
+        onChange={props.onChange}
+        sx={{ padding: 5 }}
+      />
+    </>
+  );
+}
 
 export default function DashBoard() {
   const [filter, setFilter] = React.useState('');
-
+  const { data, error, loading } = useGetProjectsQuery();
+  if (error) {
+    return (
+      <>
+        <AppBreadcrumbs items={breadcrumbItems} />
+        <AppErrorMessage message={error.message} />
+      </>
+    );
+  }
+  if (loading) {
+    return (
+      <>
+        <AppBreadcrumbs items={breadcrumbItems} />
+        <AppLoadingBar />
+      </>
+    );
+  }
+  const projects: Project[] =
+    data?.getProjects?.map((project: any) => toProject(project)) || [];
   return (
     <div>
+      <AppBreadcrumbs items={breadcrumbItems} />
       <Box sx={{ top: 0, zIndex: 1 }}>
-        <Grid container marginLeft={'5%'} alignItems="center">
-          <Grid item xs={8} md={4} lg={2} padding={1}>
-            <Typography variant="h4">Search</Typography>
-            <br />
-            <TextField
-              label="Filter"
-              value={filter}
-              onChange={(e) => setFilter(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={4} md={2} lg={1} marginLeft="auto">
-            <AddProjectButton />
-          </Grid>
-        </Grid>
+        <SearchField
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+        <AddProjectButton />
       </Box>
       <Box>
         <Grid container marginLeft={'5%'}>
+          {projects.map((project: Project) => (
+            <DashBoardItem project={project} />
+          ))}
           <Grid item xs={12}></Grid>
         </Grid>
       </Box>
@@ -38,9 +79,17 @@ function AddProjectButton() {
     <Button
       onClick={() => navigate('/mutation/addproject')}
       variant="contained"
-      sx={{ width: '100%' }}
     >
       Add Project
     </Button>
   );
+}
+function toProject(project: any): Project {
+  return {
+    projectName: project.projectName,
+    projectUrl: project.projectUrl,
+    commits: project.commits.map((commit: any) => ({
+      commitHash: commit.commitHash,
+    })),
+  };
 }
