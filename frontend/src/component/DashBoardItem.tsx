dd47116a2c47c7e7456859f3cbaa9f1d0341e354
Martin Wittlinger <wittlinger.martin@gmail.com>
2024-01-12 16:00:36 +0100

    style: Refactor Dashboard components and filter functionality
    The DashboardCard component has been deleted and its functionality has been moved into the DashBoardItem and DashBoard components. The filtering of projects on the Dashboard page is improved where now it includes both the project name and project URL. Also, minor updates were made to the Sidebar navigation menu and the layout and visuals of the Dashboard page, enhancing user experience.

--- frontend/src/component/DashBoardItem.tsx
+++ frontend/src/component/DashBoardItem.tsx
@@ -7,29 +7,43 @@ import { Project } from '../data/Project';
 interface DashBoardItemProps {
   project: Project;
 }
+
+function ProjectInfo(props: { project: Project }) {
+  return (
+    <Box marginLeft={5} marginRight={'auto'} display={'inline-block'}>
+      <Typography variant="h5" component="div">
+        {props.project.projectName}{' '}
+      </Typography>
+      <Typography variant="body1" component="div">
+        {props.project.projectUrl}{' '}
+      </Typography>
+    </Box>
+  );
+}
+
+function CommitInfo(props: { project: Project }) {
+  return (
+    <Box marginLeft={5} display={'inline-block'} alignSelf={'end'}>
+      <Typography variant="body1">
+        {props.project.commits.length} Commit(s)
+      </Typography>
+    </Box>
+  );
+}
+
 function DashBoardItem(props: DashBoardItemProps) {
   const navigate = useNavigate();
   return (
-    <>
-      <Card sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
-        <CardActionArea onClick={() => navigate(toLink(props.project))}>
-          <Avatar githubHandle={urlToGitHubHandle(props.project.projectUrl)} />
-          <Box marginLeft={5} marginRight={'auto'} display={'inline-block'}>
-            <Typography variant="h5" component="div">
-              {props.project.projectName}{' '}
-            </Typography>
-            <Typography variant="body1" component="div">
-              {props.project.projectUrl}{' '}
-            </Typography>
-          </Box>
-          <Box marginLeft={5} display={'inline-block'} alignSelf={'flex-end'}>
-            <Typography variant="body1">
-              {props.project.commits.length} Commits
-            </Typography>
-          </Box>
-        </CardActionArea>
-      </Card>
-    </>
+    <Card sx={{ display: 'flex', flexDirection: 'row', width: '70%' }}>
+      <CardActionArea
+        sx={{ display: 'flex', justifyContent: 'space-between' }}
+        onClick={() => navigate(toLink(props.project))}
+      >
+        <Avatar githubHandle={urlToGitHubHandle(props.project.projectUrl)} />
+        <ProjectInfo project={props.project} />
+        <CommitInfo project={props.project} />
+      </CardActionArea>
+    </Card>
   );
 }
 function toLink(project: Project): string {
